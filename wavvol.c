6bcbc0f402eb7e8a50562703b3eb7773699fdc4c
IagoFloriano <iagomfloriano@gmail.com>
2020-09-15 18:39:30 -0300

    input and output made still needs to change the volume

--- wavvol.c
+++ wavvol.c
@@ -1,2 +1,33 @@
 // GRR20196049 Iago Mello Floriano
+#include "wavflags.h"
+#include "wavlib.h"
 #include <stdio.h>
+
+void printUsage(char *program) {
+  fprintf(stderr, "Correct usage is %s -i input.wav -o output.wav\n", program);
+}
+
+int main(int argc, char *argv[]) {
+  WAV_t wav;
+  WavFlags_t flags;
+
+  // flags configuration
+  if (wavflags(argc, argv, "iol", &flags) == -1)
+    printUsage(argv[0]);
+  //recieve input
+  FILE *f;
+  if (flags.iFlag > 0)
+    initializeWav(argv[flags.iFlag], &wav, &f);
+  else
+    initializeWav(NULL, &wav, &f);
+
+  readAudioData(f, &wav);
+  fclose(f);
+
+  //change volume
+
+  //output
+  writeWav(stdout, &wav);
+
+  return 0;
+}
