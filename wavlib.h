0a3658344da201f5e829b82aee9bfa1e6c70b4b0
IagoFloriano <iagomfloriano@gmail.com>
2020-09-15 17:39:34 -0300

    function to get flags removed now reading inputs requires the path instead of argv and index

--- wavlib.h
+++ wavlib.h
@@ -4,6 +4,7 @@
 #ifndef __WAVLIB__
 #define __WAVLIB__
 
+// Structs for the header
 typedef struct {
   char ChunkID[4];
   uint32_t ChunkSize;
@@ -25,24 +26,47 @@ typedef struct {
   char SubChunkID[4];
   uint32_t SubChunkSize;
 } data_t;
+// End of structs for the header
 
+// The header struct
 typedef struct {
   riff_t riff;
   fmt_t fmt;
   data_t data;
 } WAVHEADER_t;
 
-/*iInput will recieve the index of the first -i flag +1
-  iOutput will recieve the index of the first -o flag +1
-  if the flag is not found the variable will recieve -1
-  the variable will also recieve -1 if is the last argument
-  NULL is a valid option if one of the flags is undesired*/
-void ioflags(int argc, char **argv, int *iInput, int *iOutput);
+// Struct for the samples
+typedef struct {
+  union {
+    uint8_t *array1b;
+    uint16_t *array2b;
+    uint32_t *array3b;
+    uint64_t *array4b;
+  };
+  int numbytes;
+  int audioSize;
+} WAVData_t;
 
-/*returns 1 if read from stdin
+// The whole wav struct;
+typedef struct {
+  WAVHEADER_t header;
+  WAVData_t data;
+} WAV_t;
+
+/*reads the header input from path
+  path should be NULL to read from stdin
+  returns 1 if able to read sucssessfully
+  returns -1 if unable to open file*/
+int readHeaderInput(char *path, WAVHEADER_t *header);
+
+/*initializes the WAVData_t struct inside the WAV_t passed usign its WAVHEADER_t*/
+int initializeWavData(WAV_t *wav);
+
+/*reads the audio data usign argv[iInput] as path
+  iInput should be -1 to read from stdin
+  returns 1 if read from stdin
   2 if read from file
-  -1 if not able to open file
-  iInput should be the index of the string to be input (-1) if stdin*/
-int readHeaderInput(int iInput, char **argv, WAVHEADER_t *header);
+  -1 if not able to open file*/
+int readAudioData(char *path, WAV_t *wav);
 
 #endif // __WAV__
