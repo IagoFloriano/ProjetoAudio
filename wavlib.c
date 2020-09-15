8223e2eec7ab86741a56749ccca5b0bf51d82199
IagoFloriano <iagomfloriano@gmail.com>
2020-09-15 18:39:01 -0300

    Initialization, reading, and outputing functions implemented

--- wavlib.c
+++ wavlib.c
@@ -9,15 +9,80 @@ void readHeader(FILE *f, WAVHEADER_t *header) {
   fread(header, sizeof(WAVHEADER_t), 1, f);
 }
 
-int readHeaderInput(char *path, WAVHEADER_t *header) {
+int readHeaderInput(char *path, WAVHEADER_t *header, FILE **f) {
   // oppen file
-  FILE *f = stdin;
+  *f = stdin;
   if (path) {
-    f = fopen(path, "r");
+    *f = fopen(path, "r");
     // test if file exists
-    if (!f)
+    if (!(*f))
       return -1;
   }
-  readHeader(f, header);
+  readHeader(*f, header);
   return 2;
+}
+
+int initializeWavData(WAV_t *wav) {
+  wav->data.numbytes = wav->header.fmt.BitsPerSample / 8;
+  wav->data.audioSize = wav->header.data.SubChunkSize / wav->data.numbytes;
+  // allocs space depending on what is needed
+  switch (wav->data.numbytes) {
+  case 1:
+    wav->data.array1b = malloc(sizeof(uint8_t) * wav->data.audioSize);
+    break;
+  case 2:
+    wav->data.array2b = malloc(sizeof(uint16_t) * wav->data.audioSize);
+    break;
+  case 3:
+    wav->data.array3b = malloc(sizeof(uint32_t) * wav->data.audioSize);
+    break;
+  case 4:
+    wav->data.array4b = malloc(sizeof(uint64_t) * wav->data.audioSize);
+    break;
+  }
+
+  //test if it was possible to allocate space and returns accordingly
+  return !(wav->data.array1b) ? -1 : 1;
+}
+
+int initializeWav(char *path, WAV_t *wav, FILE **f) {
+  if (readHeaderInput(path, &(wav->header), f) < 0)
+    return -2;
+  return initializeWavData(wav);
+}
+
+int readAudioData(FILE *f, WAV_t *wav) {
+  switch (wav->data.numbytes) {
+  case 1:
+    fread(wav->data.array1b, sizeof(uint8_t), wav->data.audioSize, f);
+    break;
+  case 2:
+    fread(wav->data.array2b, sizeof(uint16_t), wav->data.audioSize, f);
+    break;
+  case 3:
+    fread(wav->data.array3b, sizeof(uint32_t), wav->data.audioSize, f);
+    break;
+  case 4:
+    fread(wav->data.array4b, sizeof(uint64_t), wav->data.audioSize, f);
+    break;
+  }
+  return 0;
+}
+
+void writeWav(FILE *f, WAV_t *wav) {
+  fwrite(&(wav->header), sizeof(WAVHEADER_t), 1, f);
+  switch (wav->data.numbytes) {
+  case 1:
+    fwrite(wav->data.array1b, sizeof(uint8_t), wav->data.audioSize, f);
+    break;
+  case 2:
+    fwrite(wav->data.array2b, sizeof(uint16_t), wav->data.audioSize, f);
+    break;
+  case 3:
+    fwrite(wav->data.array3b, sizeof(uint32_t), wav->data.audioSize, f);
+    break;
+  case 4:
+    fwrite(wav->data.array4b, sizeof(uint64_t), wav->data.audioSize, f);
+    break;
+  }
 }
--- wavlib.h
+++ wavlib.h
@@ -43,8 +43,8 @@ typedef struct {
     uint32_t *array3b;
     uint64_t *array4b;
   };
-  int numbytes;
-  int audioSize;
+  int numbytes;  //number of bytes in each sample
+  int audioSize; //number of samples
 } WAVData_t;
 
 // The whole wav struct;
@@ -57,16 +57,27 @@ typedef struct {
   path should be NULL to read from stdin
   returns 1 if able to read sucssessfully
   returns -1 if unable to open file*/
-int readHeaderInput(char *path, WAVHEADER_t *header);
+int readHeaderInput(char *path, WAVHEADER_t *header, FILE **f);
 
-/*initializes the WAVData_t struct inside the WAV_t passed usign its WAVHEADER_t*/
+/*initializes the WAVData_t struct inside the WAV_t passed usign its WAVHEADER_t
+  this is, configures wav.data
+  allocs the space in wav.data.array#b
+  returns -1 if there was an error allocing, returns 1 otherwise*/
 int initializeWavData(WAV_t *wav);
 
-/*reads the audio data usign argv[iInput] as path
-  iInput should be -1 to read from stdin
-  returns 1 if read from stdin
-  2 if read from file
-  -1 if not able to open file*/
-int readAudioData(char *path, WAV_t *wav);
+/*reads the header from path and initializes wavdata (use NULL to read from stdin)
+  f will be the file from where it was read
+  returns 1 if there were no errors
+  returns -2 if it wasnt able to open the file
+  returns -1 if it wasnt able to allocate space for the audio*/
+int initializeWav(char *path, WAV_t *wav, FILE **f);
+
+/*reads the audio data from FILE that was already oppened
+  wav must have been initialized*/
+int readAudioData(FILE *f, WAV_t *wav);
+
+/*writes the audio in wav form on f
+  f must have been oppened before*/
+void writeWav(FILE *f, WAV_t *wav);
 
 #endif // __WAV__
