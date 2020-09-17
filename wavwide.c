0ff70b2906fa241cef644f2d7fcc162e434004fb
IagoFloriano <iagomfloriano@gmail.com>
2020-09-17 17:23:10 -0300

    Everything implemented, no debugging done

--- wavwide.c
+++ wavwide.c
@@ -7,8 +7,37 @@
 #include <stdlib.h>
 
 void printUsage(char *program) {
-  fprintf(stderr, "Program was used incorreclty, example of how to use:\n%s -i input.wav -o output.wav -l 5\n", program);
+  fprintf(stderr, "Program was used incorreclty, example of how to use:\n%s -i input.wav -o output.wav -l 5.2\n", program);
   exit(1);
+}
+
+// Aplies the desired effect
+void applyWide(WAVData_t *audio, float k) {
+  for (int i = 0; i + 1 < audio->audioSize; i += 2) {
+    int64_t diff;
+    switch (audio->numbytes) {
+    case 1:
+      diff = (audio->array1b)[i] - (audio->array1b)[i + 1];
+      (audio->array1b)[i] += k * diff;
+      (audio->array1b)[i] -= k * diff;
+      break;
+    case 2:
+      diff = (audio->array2b)[i] - (audio->array2b)[i + 1];
+      (audio->array2b)[i] += k * diff;
+      (audio->array2b)[i] -= k * diff;
+      break;
+    case 3:
+      diff = (audio->array3b)[i] - (audio->array3b)[i + 1];
+      (audio->array3b)[i] += k * diff;
+      (audio->array3b)[i] -= k * diff;
+      break;
+    case 4:
+      diff = (audio->array4b)[i] - (audio->array4b)[i + 1];
+      (audio->array4b)[i] += k * diff;
+      (audio->array4b)[i] -= k * diff;
+      break;
+    }
+  }
 }
 
 int main(int argc, char **argv) {
@@ -18,6 +47,26 @@ int main(int argc, char **argv) {
   if (wavflags(argc, argv, "i:o:l:", &flags) == -1)
     printUsage(argv[0]);
   checkInput(flags, &wav);
+
+  // Tests if audio is stereo
+  if (wav.header.fmt.NumChannels != 2) {
+    fprintf(stderr, "Input audio doesn't have 2 channels so there is nothing to be done\n");
+    free(wav.data.array1b);
+    return (2);
+  }
+
+  // Prepares variables so the effect can be applied correctly
+  float k = 1.0;
+  if (flags.lFlag) {
+    k = atof(flags.lFlag);
+    if (k > 10.0)
+      k = 10.0;
+    else if (k < 0.0)
+      k = 0.0;
+  }
+
+  // Applies the effect
+  applyWide(&(wav.data), k);
 
   // Output file
   checkOutput(flags, &wav);
